179d2e3b13d8d5c00c08ba9be7054e4b4938744a
yuta.katada@enechange.co.jp <yuta.katada@enechange.co.jp>
2024-12-22 11:59:58 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/cluster_with_only_google_maps_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cluster_with_only_google_maps_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,21 @@
+import Flutter
+import UIKit
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    let dartDefinesString = Bundle.main.infoDictionary!["DartDefines"] as! String
+    var dartDefines = [String:String]()
+    for definedValue in dartDefinesString.components(separatedBy: ",") {
+      let decoded = String(data: Data(base64Encoded: definedValue)!, encoding: .utf8)!
+      let values = decoded.components(separatedBy: "=")
+      dartDefines[values[0]] = values[1]
+    }
+    GMSServices.provideAPIKey(dartDefines["GOOGLEMAP_IOS_KEY"]!)
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
